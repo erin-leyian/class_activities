b487585dc8cb92c191ce9f3a96beffa6f1f7007f
root <e.leyian@alustudent.com>
2025-05-21 02:19:14 -0700

    activity in class

---
+++ first_script.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+name="Erin"
+echo "Hello, $name!"
+#A sample script to greet the user
+echo "What is your name?"
+read name
+echo "Hello, $name! Welcome to shell scripting."
+~                                                                                                                       ~                                                                                                                       
